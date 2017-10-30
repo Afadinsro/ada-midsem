52af79bf2b4649c197f084957e03e1986acca15f
Afadinsro <mills.josef@gmail.com>
2017-10-30 17:23:38 +0000

    Tested all methods in the country class

---
+++ CountryTest.java
@@ -0,0 +1,34 @@
+// import packages
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+/**
+ * Write a description of class CountryTestRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CountryTest
+{
+    public static void main(String[] args){
+        Country ghana = new Country("Ghana", 20000000);
+        Country nigeria = new Country("Nigeria", 100000000);
+        Country congo = new Country("Congo", 20000000);
+        
+        System.out.println("Testing getter methods...");
+        System.out.println("Ghana name: " + ghana.name());
+        System.out.println("Ghana population: " + ghana.population());
+        
+        System.out.println("Testing equals()...");
+        System.out.println("ghana=nigeria: " + ghana.equals(nigeria));
+        System.out.println("congo=ghana: " + congo.equals(ghana));
+        
+        System.out.println("Testing toString()...");
+        System.out.println(ghana);
+        
+        System.out.println("Testing compareTo()...");
+        System.out.println("ghana-nigeria: " + ghana.compareTo(nigeria));
+        System.out.println("congo-ghana: " + congo.compareTo(ghana));
+    }
+}

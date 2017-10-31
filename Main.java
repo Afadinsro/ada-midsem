574dc5af81970a6e28b21b3fe2760778848d0372
Afadinsro <mills.josef@gmail.com>
2017-10-31 17:19:38 +0000

    CountryAnalysis:sort working correctly

--- Main.java
+++ Main.java
@@ -10,6 +10,16 @@ import java.util.ArrayList;
 public class Main
 {
     public static void main(String args[]){
+        // Read in countries from file
+        ArrayList<Country> countries = CountryAnalysis.read("country_data.csv");
+        // Sort countries in increasing order of population
+        countries = CountryAnalysis.sort(countries);
+        //get number of countries
+        int size = countries.size();
+        int medianIndex = size / 2;
         
+        for(Country c: countries){
+            System.out.println(c);
+        }
     }
 }
